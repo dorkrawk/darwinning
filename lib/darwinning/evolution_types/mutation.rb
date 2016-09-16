9bc379a62d8c11d7f88957b06bc400f4fa47388d
Geoff Harcourt <geoff.harcourt@gmail.com>
2016-09-16 16:42:18 -0400

    Remove unused temporary variable assignment
    In `EvolutionTypes::Mutation#re_express_random_genotype` after the
    random genotype is selected, it gets expressed twice, with the second
    expression being assigned. The first expression is assigned to a
    temporary variable and then never accessed again.

--- lib/darwinning/evolution_types/mutation.rb
+++ lib/darwinning/evolution_types/mutation.rb
@@ -31,7 +31,6 @@ module Darwinning
       # Selects a random genotype from the organism and re-expresses its gene
       def re_express_random_genotype(member)
         random_index = (0..member.genotypes.length - 1).to_a.sample
-        new_gene = member.genes[random_index].express
         member.genotypes[random_index] = member.genes[random_index].express
         member
       end
